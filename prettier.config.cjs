d2ad351b5a960d929f15b54b31a536d00ef46be5
SwiichyCode <adlpromail@gmail.com>
2024-11-28 04:39:05 +0100

    chore: setup application

---
+++ prettier.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,3 @@
+import { handlers } from '@/config/server/auth';
+
+export const { GET, POST } = handlers;
