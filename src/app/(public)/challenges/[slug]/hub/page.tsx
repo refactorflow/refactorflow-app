5e62116d0a5cc80d9890e70cc20c3359bc540a41
SwiichyCode <adlpromail@gmail.com>
2024-11-30 20:13:13 +0100

    feat: implementing redirection logic

--- src/app/(public)/challenges/[slug]/hub/page.tsx
+++ src/app/(public)/challenges/[slug]/hub/page.tsx
@@ -1,3 +1,14 @@
-export default function ChallengeHubPage() {
-  return <div>ChallengeHubPage</div>;
+import Link from 'next/link';
+
+import { URL } from '@/config/constants/url.constant';
+import { Button } from '@/core/presentation/components/common/ui/button';
+
+export default async function ChallengeHubPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  return (
+    <Button>
+      <Link href={URL.CHALLENGE_NEW_SOLUTION(slug)}>Submit Solution</Link>
+    </Button>
+  );
 }
